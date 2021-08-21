76c3e349442051fb98190867a09f3257fd93df95
JinYunJae <jinyun3075@naver.com>
2021-08-21 19:31:16 +0900

    Initial commit

---
+++ src/main/java/com/junit/Scoreable.java
@@ -0,0 +1,6 @@
+package com.junit;
+
+@FunctionalInterface
+public interface Scoreable {
+    int getScore();
+}
---
+++ src/test/java/com/junit/ScoreCollectionTest.java
@@ -0,0 +1,27 @@
+package com.junit;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Fail.fail;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class ScoreCollectionTest {
+    @Test
+    public void test(){
+    }
+    @Test
+    public void 산술평균값_구하기(){
+        // 준비
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(()->5);
+        collection.add(()->7);
+
+        //실행
+        int actualResult = collection.arithmeticMean();
+
+        //단언
+        assertThat(actualResult, equalTo(6));
+    }
+}
